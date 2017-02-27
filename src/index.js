e5d3140430f866a30ff109f5f3da21d2c8e5865b
Su <suyang581@gmail.com>
2017-02-27 16:53:58 -0500

    Add 404 page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import LandingPage from './react/pages/LandingPage';
 import AnnouncementsContainer from './react/containers/AnnouncementsContainer';
 import CoursesContainer from './react/containers/CoursesContainer';
 import CommunityContainer from './react/containers/CommunityContainer';
+import NotFoundContainer from './react/containers/NotFoundContainer';
 
 import storeSetUp from './redux/storeSetUp';
 
@@ -19,6 +20,7 @@ ReactDOM.render(
 		  <Route path="/announcements" component={AnnouncementsContainer}/>
 		  <Route path="/courses" component={CoursesContainer}/>
 		  <Route path="/community" component={CommunityContainer}/>
+		  <Route path="*" component={NotFoundContainer}/>
 		</Router>
   </Provider>,
   document.getElementById('root')
